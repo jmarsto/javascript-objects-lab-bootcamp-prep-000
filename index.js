a5754070b7f286f284f80004ec610a419b583f49
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-14 18:20:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey(objec, key) {
-  delete
+function deleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
 }
